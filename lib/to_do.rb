cbb34b41e5f4ad4f15bfcff2e2a34a6f5ccd3f41
abdul rahimi & tyler finley <student@epicodus.com>
2015-08-11 09:54:12 -0700

    it adds a task to the array of saved tasks

--- lib/to_do.rb
+++ lib/to_do.rb
@@ -1,4 +1,5 @@
 class Todo
+  @@all_tasks = []
   define_method(:initialize) do |description|
     @description = description
   end
@@ -9,6 +10,10 @@ class Todo
   end
 
   define_singleton_method(:all) do
-    []
+    @@all_tasks
+  end
+
+  define_method(:save) do
+    @@all_tasks.push(self)
   end
 end
--- spec/to_do_spec.rb
+++ spec/to_do_spec.rb
@@ -14,4 +14,12 @@ describe(Todo) do
         expect(Todo.all()).to(eq([]))
       end
     end
+
+    describe("#save") do
+      it("adds a task to the array of saved tasks") do
+        test_task = Todo.new("throw the old milk out")
+        test_task.save()
+        expect(Todo.all()).to(eq([test_task]))
+    end
+  end
 end
